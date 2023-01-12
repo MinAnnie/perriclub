75c7a73c6d16210d0daf551d2e1f7b356a0d23bd
✨nini✨ <angiematiz1808@gmail.com>
2023-01-12 03:18:08 -0500

    archivos base del proyecto

---
+++ code/perriculb-backed/src/main/java/com/angie/perriculbbacked/PerriculbBackedApplication.java
@@ -0,0 +1,13 @@
+package com.angie.perriculbbacked;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerriculbBackedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerriculbBackedApplication.class, args);
+	}
+
+}
---
+++ code/perriculb-backed/src/test/java/com/angie/perriculbbacked/PerriculbBackedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angie.perriculbbacked;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerriculbBackedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
